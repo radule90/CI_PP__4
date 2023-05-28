ead308000eb1660b3e54749210db03047cea2539
radule90 <>
2023-05-28 16:38:04 +0000

    Add home page, view, url connect to project urls

--- member/views.py
+++ member/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    '''
+    Function based view to render home page
+    '''
+    return render(request, 'index.html')
