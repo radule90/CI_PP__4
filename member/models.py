e4c95d467a4d6c7f07657b22704e75930e02cd10
radule90 <>
2023-05-28 16:58:53 +0000

    Add member profile model, create superuser

--- member/models.py
+++ member/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class Member(models.Model):
+    '''
+    Member class for member profile that extends django User model
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    avatar = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.user
