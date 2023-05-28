7f3cf2e78da714536bc125a1d9a12cf6691cb6d5
radule90 <>
2023-05-28 17:44:55 +0000

    Add basic registration createview, path, html page

--- member/urls.py
+++ member/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import MemberCreateView
 
 
 urlpatterns = [
     path('', views.home, name='home-page'),
+    path('member/sign_up/', MemberCreateView.as_view(), name='sign-up'),
 ]
--- member/views.py
+++ member/views.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .models import Member
 
 # Create your views here.
 
@@ -8,3 +13,13 @@ def home(request):
     Function based view to render home page
     '''
     return render(request, 'index.html')
+
+
+class MemberCreateView(CreateView):
+    '''
+    Class based view for member registration
+    '''
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email', ]
+    template_name = 'member/sign_up.html'
+    success_url = reverse_lazy('home-page')
