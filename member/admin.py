d7f2a169a41a4841b958bf6f87453895536a3a0e
radule90 <>
2023-05-28 17:07:18 +0000

    Add member model to admin panel

--- member/admin.py
+++ member/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Member
 
 # Register your models here.
+
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    '''
+    Add Member model to Admin panel
+    '''
+    pass
