0a1342cde7a8d4350507857ca2d76956a81a36b5
radule90 <>
2023-05-29 07:41:16 +0000

    Add Member profile form, update member model

--- member/forms.py
+++ member/forms.py
@@ -1,6 +1,8 @@
+from cloudinary.forms import CloudinaryFileField
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Member
 
 
 class MemberSignUpForm(UserCreationForm):
@@ -10,3 +12,20 @@ class MemberSignUpForm(UserCreationForm):
         model = User
         fields = ['username', 'first_name', 'last_name',
                   'email', 'password1', 'password2']
+
+
+class MemberProfileForm(forms.ModelForm):
+    about = forms.Textarea()
+    location = forms.CharField()
+    avatar = CloudinaryFileField(
+        options={
+            'crop': 'thumb',
+            'width': 200,
+            'height': 200,
+            'folder': 'avatars'
+        }
+    )
+
+    class Meta:
+        model = Member
+        fields = ['location', 'avatar', 'about']
