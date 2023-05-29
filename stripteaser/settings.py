eb0f3421bed09d30f052697e05e8c8e942939aea
radule90 <>
2023-05-29 09:50:54 +0000

    Assign message tags in setting, add message html

--- stripteaser/settings.py
+++ stripteaser/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from django.contrib.messages import constants as messages
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
@@ -53,6 +53,14 @@ INSTALLED_APPS = [
 # Set user redirect
 LOGIN_REDIRECT_URL = '/'
 
+# Assign message tags
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
