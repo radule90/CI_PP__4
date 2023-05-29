c4327c0fe7c4325e30ca817934bd34575d9f0de7
radule90 <>
2023-05-29 22:05:10 +0000

    Add background animation style, javascript, html

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,12 @@ setTimeout(() => {
     let alert = new bootstrap.Alert(messages);
     alert.close();
 }, 2500);
+
+
+// Background Animation - Add random color to squares
+const borderColors = ['red', 'green', 'blue', 'yellow'];
+const squares = document.querySelectorAll('.animation-square');
+for (let i in squares) {
+    const randColor = borderColors[Math.floor(Math.random() * borderColors.length)];
+    squares[i].classList.add(randColor);
+}
