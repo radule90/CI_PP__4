f810205751a79e624c9f996f73c41c0c836743f3
radule90 <>
2023-05-30 14:47:28 +0000

    Create StripDetail form, from model StripDetail

---
+++ stripdetail/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import StripDetail
+from datetime import datetime
+
+
+def possible_years(first_year_in_scroll, last_year_in_scroll):
+    '''
+    Function that generates list of possible years 
+    in range based on two given arguments
+    Code is taken from
+    https://stackoverflow.com/questions/49051017/year-field-in-django
+    '''
+    p_year = []
+    for i in range(first_year_in_scroll, last_year_in_scroll, -1):
+        p_year_tuple = str(i), i
+        p_year.append(p_year_tuple)
+    return p_year
+
+
+class StripDetailForm(forms.ModelForm):
+    year = forms.ChoiceField(choices=possible_years(
+        ((datetime.now()).year), 1900),)
+
+    class Meta:
+        model = StripDetail
+        fields = ['title', 'strip_cover', 'artist', 'writer',
+                  'publisher', 'year', 'coloring', 'binding', 'country', 'pages']
