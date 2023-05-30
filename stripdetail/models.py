0058afcebf149bb07ec30200f04bab17c8b16735
radule90 <>
2023-05-30 14:12:02 +0000

    Create and migrate StripDetail model

--- stripdetail/models.py
+++ stripdetail/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from cloudinary.models import CloudinaryField
+from django_countries.fields import CountryField
+
 
 # Create your models here.
+
+
+class StripDetail(models.Model):
+    '''
+    For this model, most of the data is mandatory because it is 
+    about the technical details of the comic, in the future it will be expanded 
+    (for example, genre, language, author of the cover)
+    '''
+    COLORING_CHOICE = (('COLOR', 'Color'),
+                       ('B&W', 'Black and White'), ('DUOTONE', 'Duotone'))
+    COVER_CHOICE = (('HC', 'Hardcover'), ('SC',
+                    'Softcover'), ('TP', 'Trade Paperback'),
+                    ('FC', 'Floppy'), ('DC', 'Digital'))
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='details')
+    title = models.CharField(max_length=100, blank=False, null=False)
+    slug = models.SlugField(max_length=250, blank=False,
+                            null=False, unique=True)
+    strip_cover = CloudinaryField('cover')
+    artist = models.CharField(max_length=100, blank=False, null=False)
+    writer = models.CharField(max_length=100, blank=False, null=False)
+    publisher = models.CharField(
+        max_length=100, blank=False, null=False, unique=True)
+    year = models.PositiveIntegerField(blank=False, null=False)
+    coloring = models.CharField(max_length=50, choices=COLORING_CHOICE)
+    binding = models.CharField(
+        max_length=50, null=False, blank=False, choices=COVER_CHOICE)
+    country = CountryField(blank=False, null=False)
+    pages = models.PositiveIntegerField(blank=False, null=False)
+
+    def save(self, *args, **kwargs):
+        '''
+        Auto populate slug field solution found on:
+        https://learndjango.com/tutorials/django-slug-tutorial
+        '''
+        self.slug = slugify(self.title)
+        super(StripDetail, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.title} | {self.publisher}'
