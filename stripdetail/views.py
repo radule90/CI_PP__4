6234be6587875448d8bab2919f85b3dfc18ffaf0
radule90 <>
2023-05-30 15:00:50 +0000

    Create list view for StripDetail model

--- stripdetail/views.py
+++ stripdetail/views.py
@@ -1,3 +1,14 @@
+from .models import StripDetail
 from django.shortcuts import render
+from django.views.generic import ListView
+
 
 # Create your views here.
+class StripDetailListView(ListView):
+    '''
+    Class based view that shows list of comic strips with details
+    '''
+    model = StripDetail
+    template_name = 'stripdetail/strips.html'
+    context_object_name = 'strips'
+    paginate_by = 9
