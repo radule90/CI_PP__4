2569d7e3191e8bd4d1a9492175b18665a186f901
radule90 <>
2023-05-31 21:20:36 +0000

    Change path, url to use slug as indentifier

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -5,5 +5,5 @@ from .views import StripPostListView, StripPostCreateView, StripPostDetailView
 urlpatterns = [
     path('', StripPostListView.as_view(), name='blog'),
     path('create/', StripPostCreateView.as_view(), name='create-post'),
-    path('post/<int:pk>', StripPostDetailView.as_view(), name='post'),
+    path('post/<slug:slug>', StripPostDetailView.as_view(), name='post'),
 ]
