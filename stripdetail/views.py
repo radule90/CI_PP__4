53db3e59c08d6ca334395a0892993cf8d25a201c
radule90 <>
2023-05-31 11:49:18 +0000

    Add success messages for stripdetail views

--- stripdetail/views.py
+++ stripdetail/views.py
@@ -1,5 +1,7 @@
 from .models import StripDetail
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -19,7 +21,8 @@ class StripDetailListView(ListView):
     paginate_by = 3
 
 
-class StripDetailCreateView(LoginRequiredMixin, CreateView):
+class StripDetailCreateView(
+        LoginRequiredMixin, SuccessMessageMixin, CreateView):
     '''
     Class based view that for creating comic strips details
     form_valid override and set user of instance to current user
@@ -30,6 +33,7 @@ class StripDetailCreateView(LoginRequiredMixin, CreateView):
     template_name = 'stripdetail/strip_create.html'
     success_url = reverse_lazy('strip-list')
     login_url = 'sign-in'
+    success_message = 'Strip Successfully Added!'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -37,7 +41,8 @@ class StripDetailCreateView(LoginRequiredMixin, CreateView):
 
 
 class StripDetailUpdateView(
-        LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+        LoginRequiredMixin, UserPassesTestMixin, 
+        SuccessMessageMixin, UpdateView):
     '''
     Class based view that for updating comic strips details
     Check if user is authenticated
@@ -48,13 +53,15 @@ class StripDetailUpdateView(
     template_name = 'stripdetail/strip_update.html'
     success_url = reverse_lazy('strip-list')
     login_url = 'sign-in'
+    success_message = 'Strip Successfully Updated!'
 
     def test_func(self):
         return self.request.user == self.get_object().user
 
 
 class StripDetailDeleteView(
-        LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+        LoginRequiredMixin, UserPassesTestMixin, 
+        SuccessMessageMixin, DeleteView):
     '''
     Class based view that delete comic strip
     Tests if request user is object creator
@@ -63,6 +70,11 @@ class StripDetailDeleteView(
     template_name = 'delete_confirm.html'
     success_url = reverse_lazy('strip-list')
     login_url = 'sign-in'
+    success_message = 'Strip Successfully Deleted!'
+
+    def get_success_url(self):
+        messages.success(self.request, self.success_message)
+        return super().get_success_url()
 
     def test_func(self):
         return self.request.user == self.get_object().user
