65cf1414a6e35a563fa15871c7a0fe648297c3e3
radule90 <>
2023-05-31 21:29:04 +0000

    Create StripPostUpdateView and url

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import StripPostListView, StripPostCreateView, StripPostDetailView
+from .views import (
+    StripPostListView, StripPostCreateView, 
+    StripPostDetailView, StripPostUpdateView)
 
 
 urlpatterns = [
     path('', StripPostListView.as_view(), name='blog'),
     path('create/', StripPostCreateView.as_view(), name='create-post'),
-    path('post/<slug:slug>', StripPostDetailView.as_view(), name='post'),
+    path('post/<slug:slug>/', StripPostDetailView.as_view(), name='post'),
+    path('update/<slug:slug>/', 
+    StripPostUpdateView.as_view(), name='update-post'),
 ]
--- blogpost/views.py
+++ blogpost/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .models import StripPost
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .forms import StripPostForm
 
@@ -41,8 +41,27 @@ class StripPostCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
 
 class StripPostDetailView(DetailView):
     '''
-    Class based view for User profile details
+    Class based view for StripPost details
     '''
     model = StripPost
     template_name = 'blogpost/post.html'
     context_object_name = 'post'
+
+
+class StripPostUpdateView(
+        LoginRequiredMixin, UserPassesTestMixin, 
+        SuccessMessageMixin, UpdateView):
+    '''
+    Class based view that for updating blog posts
+    Check if user is authenticated
+    Tests if request user is object author
+    '''
+    model = StripPost
+    form_class = StripPostForm
+    template_name = 'blogpost/post_update.html'
+    success_url = reverse_lazy('blog')
+    login_url = 'sign-in'
+    success_message = 'Post Successfully Updated!'
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
