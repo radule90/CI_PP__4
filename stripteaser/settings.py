1e0ccb8261bb608147196d6fbec3e4be0003239b
radule90 <>
2023-06-07 19:18:29 +0000

    Add email configuration

--- stripteaser/settings.py
+++ stripteaser/settings.py
@@ -168,6 +168,15 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
