ce73eb5c26f051a5e86463e12818f7eca9bdc3be
radule90 <>
2023-06-07 13:55:50 +0000

    Change order of middleware

--- stripteaser/settings.py
+++ stripteaser/settings.py
@@ -98,8 +98,8 @@ MESSAGE_TAGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
