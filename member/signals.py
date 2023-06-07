a0f10eb9c22dde120beef50a668deda928bcbf8f
radule90 <>
2023-06-07 22:11:02 +0000

    Add send_mail function

--- member/signals.py
+++ member/signals.py
@@ -3,6 +3,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Member
 
+from django.core.mail import send_mail
+from django.conf import settings
+
+
 # Signals to create instance of Member, when User is created
 # Solution is taken and adapted from https://www.youtube.com/@coreyms
 
@@ -10,8 +14,16 @@ from .models import Member
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Member.objects.create(user=instance)
+        user=instance
+        member = Member.objects.create(user=user)
 
+        send_mail(
+            "Welcome to StripTeaser Blog",
+            "We value your opinion and invite you to share your reviews.",
+            settings.EMAIL_HOST_USER,
+            [member.user.email],
+            fail_silently=False,
+        )
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
